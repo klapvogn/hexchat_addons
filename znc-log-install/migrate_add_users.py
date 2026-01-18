8872fd455d2d54ccac91b9aa39d312d98fca258c
klapvogn <stefan.birkkaer.holmgaard@gmail.com>
2026-01-18 08:07:23 +0100

    Update migration script for user table creation

--- znc-log-install/migrate_add_users.py
+++ znc-log-install/migrate_add_users.py
@@ -17,8 +17,8 @@ import hashlib
 from pysqlcipher3 import dbapi2 as sqlite
 
 # Configuration - should match app.py
-DB_PATH = '/home/klapvogn/apps/znc_search/znc_logs.db'
-DB_KEY = '28ab2972b162ccc779d905cb6b422cd707d0470aef68c4289b41fa8ea42fb7df'
+DB_PATH = '/home/xxxx/apps/znc_search/znc_logs.db'
+DB_KEY = 'xxxx'
 
 def hash_password(password):
     """Hash password using SHA256"""
@@ -120,4 +120,5 @@ def migrate():
         sys.exit(1)
 
 if __name__ == '__main__':
+
     migrate()
